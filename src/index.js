3344c40f907a258f5f51d70057849b13b221036f
Nikita Chistiakov <nikita_chnv@mail.ru>
2022-09-29 23:45:30 +0300

    homework2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import "./main.scss";
 
-const add123 = (a, b) => a + b;
+let arr = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8];
 
-const res = add123(222, 3);
-console.log("Helloasda");
+function treesum(arr) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        if (Array.isArray(item)) {
+            result += treesum(item);
+        } else {
+            result += item;
+        }
+    }
+    return result;
+}
+
+console.log(treesum(arr));
+
+
+
